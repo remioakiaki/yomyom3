4cd6dad207c2d2bee6c71eb2ad3fb7add65ebba0
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-12-14 11:25:32 +0900

    users/newの追加と、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
   get 'static_pages/home'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
