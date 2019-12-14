7a972710bde5a72eaab3596d1c680dc5348ef068
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-12-14 15:37:12 +0900

    過去ソース化からの移行

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'ログイン成功'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'emailまたはパスワードに誤りがあります'
+      render 'sessions/new'
+    end
+  end
+
+  def destroy
+    logout if logged_in?
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_path
+  end
 end
