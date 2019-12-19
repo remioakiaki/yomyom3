e59ca4bb9b8829caa64c57897aacf9384a7a163d
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-12-19 14:56:02 +0900

    Micropostsコントローラーの作成

---
+++ app/models/micropost.rb
@@ -0,0 +1,27 @@
+class Micropost < ApplicationRecord
+  # validates :user_id, presence: true
+  # validates :book_id, presence: true
+  # validates :title, length: { maximum: 50 }
+  # validates :content, presence: true, length: { maximum: 140 }
+  # validates :pictures, length: { maximum: 4,
+  #                                too_long: 'は%{count}枚以内で入力してください' }
+  # validates :rate, presence: true, numericality: { greater_than: 1 }
+  belongs_to :user
+   default_scope -> { order(updated_at: :desc) }
+  # belongs_to :book
+
+   mount_uploader :picture, ImagesUploader
+   validate :picture_size
+
+  # has_many :likes, foreign_key: 'micropost_id', dependent: :destroy
+  # has_many :users, through: :likes
+
+  # has_many :comments, dependent: :destroy
+
+  private
+
+  # アップロード画像のサイズを検証する
+  def picture_size
+    errors.add(:picture, 'should be less than 5MB') if pictures.size > 5.megabytes
+  end
+end
