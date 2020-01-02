3873366747164ad91f980683183dc1900081d6ac
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-01-03 06:33:50 +0900

    commentに必要な内容の移行

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
      # get :ranking
     #end
   end
+  resources :comments, only: %i[edit update destroy]
   resources :relationships, only: %i[create destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
