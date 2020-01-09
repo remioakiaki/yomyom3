ad8dc6d3035f5317f839cd5e327262655933e615
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-01-10 07:20:33 +0900

    テストコードでエラーが出たので修正

--- spec/support/singnin_support.rb
+++ spec/support/singnin_support.rb
@@ -3,6 +3,7 @@
 module SigninSupport
   def sign_in_as(user)
     visit root_path
+    sleep 5.0
     click_link 'ログイン'
     fill_in 'session[email]', with: user.email
     fill_in 'session[password]', with: user.password
