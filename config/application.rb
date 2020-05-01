cf92374df53d143ea99fffca0eafc217934d9eff
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-01 21:54:35 +0900

    rails g でjsを作成するように設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module AppName
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.javascript_engine :js
+    end
   end
 end
