48c4773ecdb31d6d4d5755366eb864daaefb6a62
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-02 15:14:05 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20200502013237_add_references_to_bookshelves.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToBookshelves < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :bookshelves, :user, null: false, foreign_key: true
-    add_reference :bookshelves, :status, null: false, foreign_key: true
-  end
-end
