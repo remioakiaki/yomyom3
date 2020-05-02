375d14832b7ac4f68770521fe3952836be583c17
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-03 01:05:23 +0900

    test05

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, options: {
       browser: :remote,
-      url: ENV.fetch('SELENIUM_DRIVER_URL'),
+      url: http://selenium_chrome:4444/wd/hub,
       desired_capabilities: :chrome
     }
     Capybara.server_host = 'web'
