9fc3ba3c9414c1053d5056429f2245905f1d3c34
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-05 20:10:43 +0900

    日本時間設定追加

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module AppName
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Asia/Tokyo'
     config.generators do |g|
       g.javascript_engine :js
     end
