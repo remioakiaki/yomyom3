46685ce5fc11a8b40d3335461b7833cc7b75c055
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-05 20:31:33 +0900

    テストデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+User.create!(name: 'テストユーザー',
+  email: 'test@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: false)
 
+User.create!(name: '管理者ユーザー',
+ email: 'admin@admin.com',
+ password: 'administrator',
+ password_confirmation: 'administrator',
+ admin: true)
 
 5.times do |no|
   Bookshelf.create!(title: "タイトル#{no}")
