57c3f6f5b3ed9983405cc96b448481a3ea1d05e9
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-06 17:53:35 +0900

    Recordの一覧を取れるようにして、バグも修正（まだ残ってる）

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -8,7 +8,7 @@ class BookshelvesController < ApplicationController
   end
   def create
     @bookshelf = Bookshelf.find_or_initialize_by(isbn: params[:isbn])
-    return if @bookshelf.persisted?
+    #return if @bookshelf.persisted?
 
     results = RakutenWebService::Books::Book.search(isbn: @bookshelf.isbn)
     @bookshelf = Bookshelf.new(read(results.first))
@@ -19,7 +19,7 @@ class BookshelvesController < ApplicationController
   def show
     @bookshelf = Bookshelf.find(params[:id])
     @record = Record.new
-    
+    @records = Record.where(bookshelf_id: @bookshelf.id).includes(:user, :bookshelf)
   end
   private
 
