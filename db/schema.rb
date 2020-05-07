4fe9bcbd06efcf8d1edde054d5ad2423e63beaec
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-07 20:03:08 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_221946) do
+ActiveRecord::Schema.define(version: 2020_05_06_135041) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
