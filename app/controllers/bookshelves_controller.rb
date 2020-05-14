9a6ccab88a3fddb7b5f184ff6516055f799767a3
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-14 09:53:08 +0900

    bookshelvesのstatus_idとcategory_idの編集機能追加

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,2 +1,26 @@
 class BookshelvesController < ApplicationController
+
+  def edit
+    @bookshelf = Bookshelf.find(params[:id])
+    @book = Book.find(@bookshelf.id)
+    #@bookshelf = Bookshelf.eager_load(:book).where(id: params[:id])
+  end
+
+  def update
+    @bookshelf = Bookshelf.find(params[:id])
+    
+    
+    
+    if @bookshelf.update(bookshelf_params)
+      flash[:success] = '編集が完了しました'
+      redirect_back(fallback_location: root_path)
+    else
+      respond_to :js
+    end
+  end
+
+  private
+  def bookshelf_params
+    params.require(:bookshelf).permit(:status_id,:category_id)
+  end
 end
