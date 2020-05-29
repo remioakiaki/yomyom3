73dae76f5d24f2afeccf3e3e058309225e3e1fa6
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-05-29 15:37:55 +0900

    recordにもフォームからエラーが表示されるよう修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -29,9 +29,11 @@ class RecordsController < ApplicationController
   end
   def update
     @record = Record.find(params[:id])    
-    if @record.update!(record_params)
+    if @record.update(record_params)
       flash[:success] = '編集が完了しました'
       redirect_back(fallback_location: root_path)
+    else
+      respond_to :js
     end
 
   end
